562b79b70c678e16871ead4d144350c3dacc80ad
Christian Richter <christianrichter@seznam.cz>
2026-04-14 13:14:10 +0200

    Add photo references for David, Kuba, Dominik

--- seznam.js
+++ seznam.js
@@ -1,12 +1,12 @@
 const SEZNAM = [
   { name: 'Tomáš V.',  day: 28, month: 10, birthYear: 2003, photo: 'Fotky/Tomáš V..png' },
-  { name: 'David',     day:  6, month:  4, birthYear: 2001 },
+  { name: 'David',     day:  6, month:  4, birthYear: 2001, photo: 'Fotky/David.png' },
   { name: 'Naty',      day:  2, month:  6, birthYear: 1998, photo: 'Fotky/Naty.png' },
   { name: 'Chris',     day: 25, month:  7, birthYear: 1998, photo: 'Fotky/Chris.png' },
   { name: 'Káťa',      day: 10, month:  4, birthYear: 1996, photo: 'Fotky/Kata.png' },
   { name: 'Tomáš K.',  day:  2, month: 12, birthYear: 1995, photo: 'Fotky/Tomáš K..png' },
   { name: 'Kyle',      day: 21, month:  9, birthYear: 2000, photo: 'Fotky/Kyle.png' },
-  { name: 'Kuba',      day: 17, month:  2, birthYear: 1992 },
-  { name: 'Dominik',   day:  4, month:  4, birthYear: 1998 },
+  { name: 'Kuba',      day: 17, month:  2, birthYear: 1992, photo: 'Fotky/Kuba.png' },
+  { name: 'Dominik',   day:  4, month:  4, birthYear: 1998, photo: 'Fotky/Dominik.png' },
   { name: 'Kristian',  day: 18, month:  2, birthYear: 1998 },
 ];
